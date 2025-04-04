773dddcfe238f2950900384999b04e52f60c17a2
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2025-04-03 22:35:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from app.routes import routes
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://backend-est-dados.onrender.com"}})
+CORS(app, resources={r"/api/*": {"origins": "https://frontend-est-dados.vercel.app"}})
 
 # Defina um prefixo "/api" para as rotas
 app.register_blueprint(routes, url_prefix="/api")
