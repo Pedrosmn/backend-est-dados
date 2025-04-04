a8cffd2eec8c13c1c4457ae9b19e14687b481047
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2025-04-03 21:35:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from app.routes import routes
@@ -8,4 +9,5 @@ CORS(app)  # Habilita CORS para evitar bloqueios
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta fornecida pelo Render
+    app.run(host="0.0.0.0", port=port, debug=True)  # Escuta em todas as interfaces
